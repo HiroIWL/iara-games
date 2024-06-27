2a8a140a77bc18e6162896253c01fc2b5f7c3ba3
HiroIWL <hiroiwl@gmail.com>
2024-06-27 00:45:49 -0300

    Fix public assets express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const sqlite3 = require("sqlite3").verbose();
+const path = require('path')
 const db = new sqlite3.Database("./db.sqlite");
 
 const jsonParser = bodyParser.json();
@@ -9,6 +10,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(jsonParser);
 app.use(urlencodedParser);
+app.use('/assets', express.static(path.join(__dirname + "/assets")));
 
 db.serialize(() => {
     db.run(
